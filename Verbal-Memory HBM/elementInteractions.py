b1832ef6b3a01a96a18a9192932b0aef40264a17
Timdtoll <ttolstinev@gmail.com>
2025-09-08 19:43:27 -0400

    Developed main code that will automate the Verbal Memory Test on Human Benchmark. Does not currently work, as it seems that the page refreshing due to ads is interfering with finding page elements, or there is an issue with the logic.

---
+++ Verbal-Memory HBM/elementInteractions.py
@@ -0,0 +1,29 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+driver.get("https://humanbenchmark.com/tests/verbal-memory")
+
+def getWord():
+    word = driver.find_element(By.CLASS_NAME, "word").get_attribute("textContent")
+    return word
+
+def getSeenWordButton():
+    seenButton = driver.find_element(By.CLASS_NAME, "css-1bnidmp")
+    return seenButton
+
+def getNewWordButton():
+    newButton = driver.find_element(By.CLASS_NAME, "css-1bnidmp")
+    return newButton
+
+def clickStart():
+    startButton = driver.find_element(By.CLASS_NAME, "css-1bnidmp")
+    startButton.send_keys(Keys.ENTER)
+
+def clickSeen(seenButton):
+    seenButton.send_keys(Keys.ENTER)
+
+def clickNew(newButton):
+    newButton.send_keys(Keys.ENTER)
